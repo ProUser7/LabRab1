89ec336ef9a253defda8277ca1a58708be60c148
Tikhon <princeserebrynny@gmail.com>
2020-09-07 20:35:33 +0300

    Добавьте файлы проекта.

---
+++ LabRab1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LabRab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
